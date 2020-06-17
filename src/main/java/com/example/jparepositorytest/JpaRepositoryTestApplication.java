f7cc4d97b0016b5c86c5f3c76e467a804e2af4f3
Incheol Jung <bluesky761@naver.com>
2020-06-17 22:19:28 +0900

    first commit

---
+++ src/main/java/com/example/jparepositorytest/JpaRepositoryTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jparepositorytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaRepositoryTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaRepositoryTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/jparepositorytest/JpaRepositoryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jparepositorytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaRepositoryTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
